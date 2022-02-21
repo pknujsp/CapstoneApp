8679cd14d2da1b48600d93bf68d9c4c30c0aaa54
pknujsp desktop <naversovc@gmail.com>
2022-02-21 19:45:14 +0900

    캘린더 날짜내에 약속 개수 표시 작업 중

--- app/src/main/java/com/lifedawn/capstoneapp/calendar/fragments/CalendarFragment.java
+++ app/src/main/java/com/lifedawn/capstoneapp/calendar/fragments/CalendarFragment.java
@@ -14,8 +14,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.api.client.util.DateTime;
+import com.google.api.client.util.FieldInfo;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.Events;
 import com.kizitonwose.calendarview.model.CalendarDay;
 import com.kizitonwose.calendarview.model.CalendarMonth;
@@ -55,190 +58,263 @@ import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 
 public class CalendarFragment extends Fragment {
-    private FragmentCalendarBinding binding;
-    private AccountViewModel accountViewModel;
-    private CalendarViewModel calendarViewModel;
-    private final DayOfWeek FIRST_DAY_OF_WEEK = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
-    private final Map<String, List<Event>> eventsMap = new HashMap<>();
+	private FragmentCalendarBinding binding;
+	private AccountViewModel accountViewModel;
+	private CalendarViewModel calendarViewModel;
+	private final DayOfWeek FIRST_DAY_OF_WEEK = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+	private final Map<String, List<Event>> eventsMap = new HashMap<>();
 
-    private YearMonth currentMonth = YearMonth.now(ZoneId.systemDefault());
-    private YearMonth firstMonth = currentMonth.minusMonths(10);
-    private YearMonth lastMonth = currentMonth.plusMonths(10);
+	private static final String ACCEPTED = "accepted";
+	private static final String NEEDS_ACTION = "needsAction";
+	private static final String DECLINED = "declined";
+	private String myEmail;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        accountViewModel = new ViewModelProvider(getActivity()).get(AccountViewModel.class);
-        calendarViewModel = new ViewModelProvider(getActivity()).get(CalendarViewModel.class);
-    }
+	private ZonedDateTime firstDateTime;
+	private ZonedDateTime endDateTime;
+	private final ZoneId zoneId = ZoneId.systemDefault();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentCalendarBinding.inflate(inflater);
-        return binding.getRoot();
-    }
+	private YearMonth currentMonth = YearMonth.now(ZoneId.systemDefault());
+	private YearMonth firstMonth = currentMonth.minusMonths(10);
+	private YearMonth lastMonth = currentMonth.plusMonths(10);
 
-    @Override
-    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        binding.calendarView.setup(firstMonth, lastMonth, FIRST_DAY_OF_WEEK);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		accountViewModel = new ViewModelProvider(getActivity()).get(AccountViewModel.class);
+		calendarViewModel = new ViewModelProvider(getActivity()).get(CalendarViewModel.class);
+	}
 
-        binding.calendarView.setMonthHeaderBinder(new MonthHeaderFooterBinder<MonthHeaderViewContainer>() {
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy M");
-            ZonedDateTime zonedDateTime = ZonedDateTime.now();
-            final DayOfWeek[] dayOfWeeks = new DayOfWeek[]{DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
-                    DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY};
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		binding = FragmentCalendarBinding.inflate(inflater);
+		return binding.getRoot();
+	}
 
-            ZonedDateTime firstDateTime;
-            ZonedDateTime endDateTime;
-            final ZoneId zoneId = ZoneId.systemDefault();
+	@Override
+	public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		myEmail = accountViewModel.lastSignInAccount().getEmail();
+		binding.calendarView.setup(firstMonth, lastMonth, FIRST_DAY_OF_WEEK);
 
-            @NonNull
-            @Override
-            public MonthHeaderViewContainer create(@NonNull View view) {
-                return new MonthHeaderViewContainer(view);
-            }
+		binding.calendarView.setMonthHeaderBinder(new MonthHeaderFooterBinder<MonthHeaderViewContainer>() {
+			DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy M");
+			ZonedDateTime zonedDateTime = ZonedDateTime.now();
+			final DayOfWeek[] dayOfWeeks = new DayOfWeek[]{DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY, DayOfWeek.WEDNESDAY,
+					DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY};
 
-            // 달력 상단 바인딩
-            @Override
-            public void bind(@NonNull MonthHeaderViewContainer monthHeaderViewContainer, @NonNull CalendarMonth calendarMonth) {
-                zonedDateTime = zonedDateTime.withYear(calendarMonth.getYear());
-                zonedDateTime = zonedDateTime.withMonth(calendarMonth.getMonth());
-                monthHeaderViewContainer.binding.headerTextView.setText(zonedDateTime.format(dateTimeFormatter));
+			@NonNull
+			@Override
+			public MonthHeaderViewContainer create(@NonNull View view) {
+				return new MonthHeaderViewContainer(view);
+			}
 
-                if (monthHeaderViewContainer.binding.days.getTag() == null) {
-                    monthHeaderViewContainer.binding.days.setTag(Boolean.TRUE);
-                    int childCount = monthHeaderViewContainer.binding.days.getChildCount();
-                    for (int child = 0; child < childCount; child++) {
-                        ((TextView) monthHeaderViewContainer.binding.days.getChildAt(child)).setText(
-                                dayOfWeeks[child].getDisplayName(TextStyle.SHORT, Locale.getDefault()));
-                    }
-                }
+			// 달력 상단 바인딩
+			@Override
+			public void bind(@NonNull MonthHeaderViewContainer monthHeaderViewContainer, @NonNull CalendarMonth calendarMonth) {
+				zonedDateTime = zonedDateTime.withYear(calendarMonth.getYear());
+				zonedDateTime = zonedDateTime.withMonth(calendarMonth.getMonth());
+				monthHeaderViewContainer.binding.headerTextView.setText(zonedDateTime.format(dateTimeFormatter));
 
-                firstDateTime = ZonedDateTime.of(calendarMonth.getYear(), calendarMonth.getMonth(), 1, 0, 0, 0, 0, zoneId);
-                endDateTime = firstDateTime.plusMonths(1);
+				if (monthHeaderViewContainer.binding.days.getTag() == null) {
+					monthHeaderViewContainer.binding.days.setTag(Boolean.TRUE);
+					int childCount = monthHeaderViewContainer.binding.days.getChildCount();
+					for (int child = 0; child < childCount; child++) {
+						((TextView) monthHeaderViewContainer.binding.days.getChildAt(child)).setText(
+								dayOfWeeks[child].getDisplayName(TextStyle.SHORT, Locale.getDefault()));
+					}
+				}
 
-                DateTime timeMin = new DateTime(firstDateTime.toInstant().getEpochSecond() * 1000L);
-                DateTime timeMax = new DateTime(endDateTime.toInstant().getEpochSecond() * 1000L);
-                loadEvents(timeMin, timeMax, new BackgroundCallback<List<Event>>() {
-                    @Override
-                    public void onResultSuccessful(List<Event> events) {
-                        if (getActivity() != null) {
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
 
-                                }
-                            });
-                        }
-                    }
+			}
+		});
 
-                    @Override
-                    public void onResultFailed(Exception e) {
+		// 날짜표시와 밑에 파란색으로 약속 카운팅도 하기
+		binding.calendarView.setDayBinder(new DayBinder<DayViewContainer>() {
+			String dateText;
+			List<Event> eventList;
+			List<EventAttendee> attendeeList;
+			Integer acceptedCount;
+			Integer declinedCount;
+			Integer needsActionCount;
 
-                    }
-                });
-            }
-        });
+			@NonNull
+			@Override
+			public DayViewContainer create(@NonNull View view) {
+				return new DayViewContainer(view);
+			}
 
-        // 날짜표시와 밑에 파란색으로 약속 카운팅도 하기
-        binding.calendarView.setDayBinder(new DayBinder<DayViewContainer>() {
+			@Override
+			public void bind(@NonNull DayViewContainer viewContainer, @NonNull CalendarDay calendarDay) {
+				viewContainer.binding.calendarDayText.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
+				if (calendarDay.getOwner() == DayOwner.THIS_MONTH) {
+					viewContainer.binding.calendarDayText.setTextColor(Color.BLACK);
+				} else {
+					viewContainer.binding.calendarDayText.setTextColor(Color.LTGRAY);
+				}
 
-            @NonNull
-            @Override
-            public DayViewContainer create(@NonNull View view) {
-                return new DayViewContainer(view);
-            }
+				viewContainer.binding.calendarDayText.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Toast.makeText(getContext(), String.valueOf(calendarDay.getDate().getDayOfMonth()), Toast.LENGTH_SHORT).show();
+					}
+				});
+				acceptedCount = 0;
+				needsActionCount = 0;
 
-            @Override
-            public void bind(@NonNull DayViewContainer viewContainer, @NonNull CalendarDay calendarDay) {
-                viewContainer.binding.calendarDayText.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
-                if (calendarDay.getOwner() == DayOwner.THIS_MONTH) {
-                    viewContainer.binding.calendarDayText.setTextColor(Color.BLACK);
-                } else {
-                    viewContainer.binding.calendarDayText.setTextColor(Color.LTGRAY);
-                }
+				dateText = calendarDay.getDate().toString();
+				if (eventsMap.containsKey(dateText)) {
+					eventList = eventsMap.get(dateText);
 
-                viewContainer.binding.calendarDayText.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(getContext(), String.valueOf(calendarDay.getDate().getDayOfMonth()), Toast.LENGTH_SHORT).show();
-                    }
-                });
+					for (Event event : eventList) {
+						attendeeList = event.getAttendees();
 
-            }
-        });
+						if (attendeeList != null) {
+							for (EventAttendee attendee : attendeeList) {
 
-        binding.calendarView.setMonthScrollListener(new Function1<CalendarMonth, Unit>() {
-            @Override
-            public Unit invoke(CalendarMonth calendarMonth) {
-                if ((calendarMonth.getYearMonth().compareTo(firstMonth) <= 3) || (calendarMonth.getYearMonth().compareTo(
-                        lastMonth) >= -3)) {
-                    firstMonth = firstMonth.minusMonths(10);
-                    lastMonth = lastMonth.plusMonths(10);
-                    binding.calendarView.updateMonthRangeAsync(firstMonth, lastMonth);
-                }
-                return null;
-            }
-        });
+								if (attendee.getEmail().equals(myEmail)) {
+									if (attendee.getResponseStatus().equals(ACCEPTED)) {
+										acceptedCount++;
+									}
+									if (attendee.getResponseStatus().equals(NEEDS_ACTION)) {
+										needsActionCount++;
+									} else {
 
-        binding.calendarView.scrollToMonth(currentMonth);
-    }
+									}
+								}
 
-    private void loadEvents(DateTime timeMin, DateTime timeMax, BackgroundCallback<List<Event>> callback) {
-        MyApplication.EXECUTOR_SERVICE.execute(new Runnable() {
-            @Override
-            public void run() {
-                final Calendar calendarService = calendarViewModel.getCalendarService();
-                final List<Event> eventList = new ArrayList<>();
-                String pageToken = null;
+							}
+						}
 
-                final String[] calendarIds = new String[]{calendarViewModel.getMainCalendarId(), "primary"};
+					}
 
-                try {
-                    for (String calendarId : calendarIds) {
-                        pageToken = null;
-                        do {
-                            Events events = calendarService.events().list(calendarId).setPageToken(pageToken).setTimeMin(timeMin)
-                                    .setTimeMax(timeMax).execute();
-                            eventList.addAll(events.getItems());
 
-                            pageToken = events.getNextPageToken();
-                        } while (pageToken != null);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+				}
+				if (acceptedCount > 0) {
+					viewContainer.binding.acceptedPromiseCount.setText(acceptedCount.toString());
+				} else {
+					viewContainer.binding.acceptedPromiseCount.setText(null);
+				}
+				if (needsActionCount > 0) {
+					viewContainer.binding.needsActionPromiseCount.setText(needsActionCount.toString());
+				} else {
+					viewContainer.binding.needsActionPromiseCount.setText(null);
+				}
+			}
+		});
 
-                final String date = timeMin.toStringRfc3339();
-                eventsMap.put(date, eventList);
-                callback.onResultSuccessful(eventList);
-            }
-        });
+		binding.calendarView.setMonthScrollListener(new Function1<CalendarMonth, Unit>() {
+			@Override
+			public Unit invoke(CalendarMonth calendarMonth) {
+				if ((calendarMonth.getYearMonth().compareTo(firstMonth) <= 3) || (calendarMonth.getYearMonth().compareTo(
+						lastMonth) >= -3)) {
+					loadEvents(calendarMonth.getYear(), calendarMonth.getMonth(), new BackgroundCallback<List<Event>>() {
+						@Override
+						public void onResultSuccessful(List<Event> e) {
+							binding.calendarView.notifyCalendarChanged();
+						}
 
-    }
+						@Override
+						public void onResultFailed(Exception e) {
 
-    // 일 보여주는 컨테이너 바인딩
-    private class DayViewContainer extends ViewContainer {
-        private CalendarDayLayoutBinding binding;
+						}
+					});
+					firstMonth = firstMonth.minusMonths(10);
+					lastMonth = lastMonth.plusMonths(10);
+					binding.calendarView.updateMonthRangeAsync(firstMonth, lastMonth);
+				}
+				return null;
+			}
+		});
 
-        public DayViewContainer(@NonNull View view) {
-            super(view);
-            binding = CalendarDayLayoutBinding.bind(view);
+		binding.calendarView.scrollToMonth(currentMonth);
+		loadEvents(currentMonth.getYear(), currentMonth.getMonthValue(), new BackgroundCallback<List<Event>>() {
+			@Override
+			public void onResultSuccessful(List<Event> e) {
+				binding.calendarView.notifyCalendarChanged();
+			}
 
-        }
+			@Override
+			public void onResultFailed(Exception e) {
 
-    }
+			}
+		});
+	}
 
-    // 달 보여주는 컨테이너 바인딩
-    private class MonthHeaderViewContainer extends ViewContainer {
-        private CalendarMonthHeaderLayoutBinding binding;
+	private void loadEvents(int year, int month, BackgroundCallback<List<Event>> callback) {
+		MyApplication.EXECUTOR_SERVICE.execute(new Runnable() {
+			@Override
+			public void run() {
+				firstDateTime = ZonedDateTime.of(year, month, 1, 0, 0, 0, 0, zoneId);
+				endDateTime = firstDateTime.plusMonths(1);
 
-        public MonthHeaderViewContainer(@NonNull View view) {
-            super(view);
-            this.binding = CalendarMonthHeaderLayoutBinding.bind(view);
-        }
-    }
+				DateTime timeMin = new DateTime(firstDateTime.toInstant().getEpochSecond() * 1000L);
+				DateTime timeMax = new DateTime(endDateTime.toInstant().getEpochSecond() * 1000L);
+
+				final Calendar calendarService = calendarViewModel.getCalendarService();
+				final List<Event> eventList = new ArrayList<>();
+				String pageToken = null;
+
+				final String[] calendarIds = new String[]{calendarViewModel.getMainCalendarId(), "primary"};
+
+				try {
+					for (String calendarId : calendarIds) {
+						pageToken = null;
+						do {
+							Events events = calendarService.events().list(calendarId).setPageToken(pageToken).setTimeMin(timeMin)
+									.setTimeMax(timeMax).execute();
+							eventList.addAll(events.getItems());
+
+							pageToken = events.getNextPageToken();
+						} while (pageToken != null);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					callback.onResultFailed(e);
+				}
+
+				String dateText = null;
+				EventDateTime eventDateTime = null;
+				ZonedDateTime start = null;
+
+				for (Event event : eventList) {
+					eventDateTime = event.getStart();
+					start = ZonedDateTime.parse(eventDateTime.getDateTime().toString());
+					start = start.withZoneSameInstant(ZoneId.of(eventDateTime.getTimeZone()));
+					dateText = start.toLocalDate().toString();
+
+					if (!eventsMap.containsKey(dateText)) {
+						eventsMap.put(dateText, new ArrayList<>());
+					}
+					eventsMap.get(dateText).add(event);
+				}
+
+				callback.onResultSuccessful(eventList);
+			}
+		});
+
+	}
+
+
+	// 일 보여주는 컨테이너 바인딩
+	private static class DayViewContainer extends ViewContainer {
+		private CalendarDayLayoutBinding binding;
+
+		public DayViewContainer(@NonNull View view) {
+			super(view);
+			binding = CalendarDayLayoutBinding.bind(view);
+
+		}
+	}
+
+	// 달 보여주는 컨테이너 바인딩
+	private static class MonthHeaderViewContainer extends ViewContainer {
+		private CalendarMonthHeaderLayoutBinding binding;
+
+		public MonthHeaderViewContainer(@NonNull View view) {
+			super(view);
+			this.binding = CalendarMonthHeaderLayoutBinding.bind(view);
+		}
+	}
 
 
 }
